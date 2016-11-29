f2f0bc4e41fb68dcc012cb28c5428027e22871ac
Kirstin <kirstinalexis@gmail.com>
2016-11-28 21:42:14 -0600

    Update outfitcounter.py

--- outfitcounter.py
+++ outfitcounter.py
@@ -5,14 +5,21 @@ def outfit_counter(tops, bottoms, dresses):
         print "%s of your outfits are dresses.\n" % (dresses)
     return outfits
 
-tops = int(raw_input("How many tops? "))
-bottoms = int(raw_input("How many bottoms? "))
-dresses = int(raw_input("How many dresses? "))
+tops = float(raw_input("How many tops? "))
+bottoms = float(raw_input("How many bottoms? "))
+dresses = float(raw_input("How many dresses? "))
 outfits = outfit_counter(tops, bottoms, dresses)
 
-laundry = int(raw_input("How many times will you do laundry? "))
-if laundry == 0:
-    laundry = 1
-    print "If you don't do laundry, you will have %s outfits." % (outfits)
+days = raw_input("How many days are you packing for?")
+if days <= outfits:
+    print "You will not need to do laundry."
 else:
-    print "If you do laundry %s times, you will have %s outfits." % (laundry, (laundry+1)*outfits)
+    laundry_trips = days / outfits
+    print "You will need to do laundry %s times." % (laundry_trips)
+
+#laundry = int(raw_input("How many times will you do laundry? "))
+#if laundry == 0:
+ #   laundry = 1
+  #  print "If you don't do laundry, you will have %s outfits." % (outfits)
+#else:
+ #   print "If you do laundry %s times, you will have %s outfits." % (laundry, (laundry+1)*outfits)
